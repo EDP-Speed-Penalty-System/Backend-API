f14910c2b33c207755afbd143a15d164e520f964
Ujjawal Lele <ujjawallele13@gmail.com>
2023-04-19 23:10:34 +0530

    now no need to pass lat and long in the url

--- speed/urls.py
+++ speed/urls.py
@@ -8,10 +8,9 @@ urlpatterns = [
     re_path(r'^auth/logout/', views.logout, name='logout-api'),
     re_path(r'^profile/(?P<username>.+)/', views.profile, name='profile-api'),
     re_path(r'^profile/', views.profile, name='profile-api'),
-    # re_path(r'^speed_limit/(?P<lat>[0-9]+\.?[0-9])/(?P<long>[0-9]+\.?[0-9])', views.get_speed_limit,name='speed_limit-detail-get-api'),
-    re_path(r'^speed_limit/(?P<lat>[-+]?[0-9]*\.?[0-9]+)/(?P<long>[-+]?[0-9]*\.?[0-9]+)/$', views.get_speed_limit, name='speed_limit-detail-get-api'),
+    re_path(r'^speed_limit/$', views.get_speed_limit, name='speed_limit-detail-get-api'),
+    # re_path(r'^speed_limit/(?P<lat>[-+]?[0-9]*\.?[0-9]+)/(?P<long>[-+]?[0-9]*\.?[0-9]+)/$', views.get_speed_limit, name='speed_limit-detail-get-api'),
     re_path(r'^penalty/', views.penalty, name='penalty'),
     # re_path(r'^penalty/', views.StudentList.as_view(), name='penalty'),
-    # path('example/<float:lat>/<float:long>/', views.get_speed_limit, name='example'),
 
 ]
--- speed/views.py
+++ speed/views.py
@@ -70,9 +70,11 @@ def profile(request, username=None):
 @api_view(['GET'])
 @permission_classes([IsAuthenticated])
 @authentication_classes([TokenAuthentication])
-def get_speed_limit(request,lat, long):
+def get_speed_limit(request):
     # complaint_detail = StudentComplain.objects.get(id=detailcomp_id1)
     speed_detail = SpeedLimit.objects.all()
+    lat = request.headers.get('lat')
+    long = request.headers.get('long')
     point = (lat, long)
   
 # Print the distance calculated in km
@@ -87,6 +89,9 @@ def get_speed_limit(request,lat, long):
     response = {
         'speed_limit' : speed
     }
+    # print(lat)
+    # print(long)
+    print(response)
     return Response(data=response,status=status.HTTP_200_OK)
 
 
@@ -134,199 +139,4 @@ def penalty(request, username=None):
 #             return Response(serializer.data,status=status.HTTP_201_CREATED)
 #         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST) 
 
-
-
-# @api_view(['GET'])
-# @permission_classes([IsAuthenticated])
-# @authentication_classes([TokenAuthentication])
-# def student_complain_api(request):
-#     user = get_object_or_404(User,username = request.user.username)
-#     user = ExtraInfo.objects.all().filter(user = user).first()
-#     if user.user_type == 'student':
-#         complain = StudentComplain.objects.filter(complainer = user)
-#     elif user.user_type == 'staff':
-#         staff = ExtraInfo.objects.get(id=user.id)
-#         staff = Caretaker.objects.get(staff_id=staff)
-#         complain = StudentComplain.objects.filter(location = staff.area)
-#     elif user.user_type == 'faculty':
-#         faculty = ExtraInfo.objects.get(id=user.id)
-#         faculty = Supervisor.objects.get(sup_id=faculty)
-#         complain = StudentComplain.objects.filter(location = faculty.area)
-#     complains = serializers.StudentComplainSerializers(complain,many=True).data
-#     resp = {
-#         'student_complain' : complains,
-#     }
-#     return Response(data=resp,status=status.HTTP_200_OK)
-
-# @api_view(['POST'])
-# @permission_classes([IsAuthenticated])
-# @authentication_classes([TokenAuthentication])
-# def create_complain_api(request):
-#     serializer = serializers.StudentComplainSerializers(data=request.data)
-#     if serializer.is_valid():
-#         serializer.save()
-#         return Response(serializer.data, status=status.HTTP_201_CREATED)
-#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-# @api_view(['DELETE','PUT'])
-# @permission_classes([IsAuthenticated])
-# @authentication_classes([TokenAuthentication])
-# def edit_complain_api(request,c_id):
-#     try: 
-#         complain = StudentComplain.objects.get(id = c_id) 
-#     except StudentComplain.DoesNotExist: 
-#         return Response({'message': 'The Complain does not exist'}, status=status.HTTP_404_NOT_FOUND)
-#     if request.method == 'DELETE':
-#         complain.delete()
-#         return Response({'message': 'Complain deleted'},status=status.HTTP_404_NOT_FOUND)
-#     elif request.method == 'PUT':
-#         serializer = serializers.StudentComplainSerializers(complain,data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data,status=status.HTTP_200_OK)
-#         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-
-# @api_view(['GET','POST'])
-# @permission_classes([IsAuthenticated])
-# @authentication_classes([TokenAuthentication])
-# def worker_api(request):
-
-#     if request.method == 'GET':
-#         worker = Workers.objects.all()
-#         workers = serializers.WorkersSerializers(worker,many=True).data
-#         resp = {
-#             'workers' : workers,
-#         }
-#         return Response(data=resp,status=status.HTTP_200_OK)
-
-#     elif request.method =='POST':
-#         user = get_object_or_404(User ,username=request.user.username)
-#         user = ExtraInfo.objects.all().filter(user = user).first()
-#         try :
-#             caretaker = Caretaker.objects.get(staff_id=user)
-#         except Caretaker.DoesNotExist:
-#             return Response({'message':'Logged in user does not have the permissions'},status=status.HTTP_203_NON_AUTHORITATIVE_INFORMATION)
-#         serializer = serializers.WorkersSerializers(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-# @api_view(['DELETE','PUT'])
-# @permission_classes([IsAuthenticated])
-# @authentication_classes([TokenAuthentication])
-# def edit_worker_api(request,w_id):
-#     user = get_object_or_404(User ,username=request.user.username)
-#     user = ExtraInfo.objects.all().filter(user = user).first()
-#     try :
-#         caretaker = Caretaker.objects.get(staff_id=user)
-#     except Caretaker.DoesNotExist:
-#         return Response({'message':'Logged in user does not have the permissions'},status=status.HTTP_203_NON_AUTHORITATIVE_INFORMATION)
-#     try: 
-#         worker = Workers.objects.get(id = w_id) 
-#     except Workers.DoesNotExist: 
-#         return Response({'message': 'The worker does not exist'}, status=status.HTTP_404_NOT_FOUND)
-#     if request.method == 'DELETE':
-#         worker.delete()
-#         return Response({'message': 'Worker deleted'},status=status.HTTP_404_NOT_FOUND)
-#     elif request.method == 'PUT':
-#         serializer = serializers.WorkersSerializers(worker,data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data,status=status.HTTP_200_OK)
-#         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-
-# @api_view(['GET','POST'])
-# @permission_classes([IsAuthenticated])
-# @authentication_classes([TokenAuthentication])
-# def caretaker_api(request):
-
-#     if request.method == 'GET':
-#         caretaker = Caretaker.objects.all()
-#         caretakers = serializers.CaretakerSerializers(caretaker,many=True).data
-#         resp = {
-#             'caretakers' : caretakers,
-#         }
-#         return Response(data=resp,status=status.HTTP_200_OK)
-    
-#     elif request.method == 'POST':
-#         user = get_object_or_404(User ,username=request.user.username)
-#         user = ExtraInfo.objects.all().filter(user = user).first()
-#         try :
-#             supervisor = Supervisor.objects.get(sup_id=user)
-#         except Supervisor.DoesNotExist:
-#             return Response({'message':'Logged in user does not have the permissions'},status=status.HTTP_203_NON_AUTHORITATIVE_INFORMATION)
-#         serializer = serializers.CaretakerSerializers(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data,status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST) 
-
-# @api_view(['DELETE','PUT'])
-# @permission_classes([IsAuthenticated])
-# @authentication_classes([TokenAuthentication])
-# def edit_caretaker_api(request,c_id):
-#     user = get_object_or_404(User ,username=request.user.username)
-#     user = ExtraInfo.objects.all().filter(user = user).first()
-#     try :
-#         supervisor = Supervisor.objects.get(sup_id=user)
-#     except Supervisor.DoesNotExist:
-#         return Response({'message':'Logged in user does not have the permissions'},status=status.HTTP_203_NON_AUTHORITATIVE_INFORMATION)
-#     try: 
-#         caretaker = Caretaker.objects.get(id = c_id) 
-#     except Caretaker.DoesNotExist: 
-#         return Response({'message': 'The Caretaker does not exist'}, status=status.HTTP_404_NOT_FOUND)
-#     if request.method == 'DELETE':
-#         caretaker.delete()
-#         return Response({'message': 'Caretaker deleted'},status=status.HTTP_404_NOT_FOUND)
-#     elif request.method == 'PUT':
-#         serializer = serializers.CaretakerSerializers(caretaker,data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data,status=status.HTTP_200_OK)
-#         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-
-# @api_view(['GET','POST'])
-# @permission_classes([IsAuthenticated])
-# @authentication_classes([TokenAuthentication])
-# def supervisor_api(request):
-
-#     if request.method == 'GET':
-#         supervisor = Supervisor.objects.all()
-#         supervisors = serializers.SupervisorSerializers(supervisor,many=True).data
-#         resp = {
-#             'supervisors' : supervisors,
-#         }
-#         return Response(data=resp,status=status.HTTP_200_OK)
-    
-#     elif request.method == 'POST':
-#         user = get_object_or_404(User,username=request.user.username)
-#         if user.is_superuser == False:
-#             return Response({'message':'Logged in user does not have permission'},status=status.HTTP_203_NON_AUTHORITATIVE_INFORMATION)
-#         serializer = serializers.SupervisorSerializers(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data,status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-   
-# @api_view(['DELETE','PUT'])
-# @permission_classes([IsAuthenticated])
-# @authentication_classes([TokenAuthentication])
-# def edit_supervisor_api(request,s_id):
-#     user = get_object_or_404(User,username=request.user.username)
-#     if user.is_superuser == False:
-#         return Response({'message':'Logged in user does not have permission'},status=status.HTTP_203_NON_AUTHORITATIVE_INFORMATION)
-#     try: 
-#         supervisor = Supervisor.objects.get(id = s_id) 
-#     except Supervisor.DoesNotExist: 
-#         return Response({'message': 'The Caretaker does not exist'}, status=status.HTTP_404_NOT_FOUND)
-#     if request.method == 'DELETE':
-#         supervisor.delete()
-#         return Response({'message': 'Caretaker deleted'},status=status.HTTP_404_NOT_FOUND)
-#     elif request.method == 'PUT':
-#         serializer = serializers.SupervisorSerializers(supervisor,data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data,status=status.HTTP_200_OK)
-#         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
 
